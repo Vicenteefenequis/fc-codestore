120fedee800ccb7d52cf3d6e2c5f0f0941fc110a
vicente_nascimento <vicente19981@live.com>
2023-01-11 11:52:53 -0300

    refactor: change required passing id on base entity

--- src/modules/@shared/domain/value-object/id.value-object.ts
+++ src/modules/@shared/domain/value-object/id.value-object.ts
@@ -1,14 +1,14 @@
-import { v4 as uuid } from 'uuid'
+import { v4 as uuid } from "uuid";
 import ValueObject from "./value-object.interface";
 
 export default class Id implements ValueObject {
-    private _id: string;
+  private _id: string;
 
-    constructor(id: string) {
-        this._id = id || uuid();
-    }
+  constructor(id?: string) {
+    this._id = id || uuid();
+  }
 
-    get id() {
-        return this._id;
-    }
+  get id() {
+    return this._id;
+  }
 }
