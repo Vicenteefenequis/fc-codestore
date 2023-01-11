2be3d64d7afb21a4daa9ba451d5e8e41dfb0e097
vicente_nascimento <vicente19981@live.com>
2023-01-10 23:48:01 -0300

    refactor: change execute of product use case

--- src/modules/product-adm/usecase/add-product/add-product.usecase.ts
+++ src/modules/product-adm/usecase/add-product/add-product.usecase.ts
@@ -4,22 +4,26 @@ import ProductGateway from "../../gateway/product.gateway";
 import { InputAddProductDto, OutputAddProductDto } from "./add-product.dto";
 
 export default class AddProductUseCase {
-    constructor(private readonly productGateway: ProductGateway) {
-    }
+  constructor(private readonly productGateway: ProductGateway) {}
 
+  async execute(input: InputAddProductDto): Promise<OutputAddProductDto> {
+    const product = new Product({
+      id: new Id(input.id),
+      description: input.description,
+      name: input.name,
+      purchasePrice: input.purchasePrice,
+      stock: input.stock,
+    });
+    await this.productGateway.add(product);
 
-    async execute(input: InputAddProductDto): Promise<OutputAddProductDto> {
-        const product = new Product({ id: new Id(input.id), description: input.description, name: input.name, purchasePrice: input.purchasePrice, stock: input.stock })
-        const result = await this.productGateway.addProduct(product);
-
-        return {
-            id: product.id.id,
-            description: product.description,
-            name: product.name,
-            purchasePrice: product.purchasePrice,
-            stock: product.stock,
-            createdAt: product.createdAt,
-            updatedAt: product.updatedAt
-        };
-    }
+    return {
+      id: product.id.id,
+      description: product.description,
+      name: product.name,
+      purchasePrice: product.purchasePrice,
+      stock: product.stock,
+      createdAt: product.createdAt,
+      updatedAt: product.updatedAt,
+    };
+  }
 }
