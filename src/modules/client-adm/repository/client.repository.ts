baad01a3c382a01d3651f3d92f9ce90403e62f0a
vicente_nascimento <vicente19981@live.com>
2023-01-11 12:27:30 -0300

    feat: implementing add client repository

--- src/modules/client-adm/repository/client.repository.ts
+++ src/modules/client-adm/repository/client.repository.ts
@@ -4,7 +4,16 @@ import ClientGateway from "../gateway/client.gateway";
 import ClientModel from "./client.model";
 
 export default class ClientRepository implements ClientGateway {
-  async add(client: Client): Promise<void> {}
+  async add(client: Client): Promise<void> {
+    await ClientModel.create({
+      id: client.id.id,
+      name: client.name,
+      email: client.email,
+      address: client.address,
+      createdAt: client.createdAt,
+      updatedAt: client.updatedAt,
+    });
+  }
 
   async find(id: string): Promise<Client> {
     const client = await ClientModel.findOne({ where: { id } });
