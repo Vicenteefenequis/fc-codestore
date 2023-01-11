40d3a11b316b99a1cadc82c09ba0f71f61b4cf5c
vicente_nascimento <vicente19981@live.com>
2023-01-11 12:27:41 -0300

    test: ensure create a client

--- src/modules/client-adm/repository/client.repository.spec.ts
+++ src/modules/client-adm/repository/client.repository.spec.ts
@@ -1,4 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
+import Id from "../../@shared/domain/value-object/id.value-object";
+import Client from "../domain/client.entity";
 import ClientModel from "./client.model";
 import ClientRepository from "./client.repository";
 
@@ -20,6 +22,31 @@ describe("ClientRepository test", () => {
 
   afterEach(async () => {
     await sequelize.close();
+  });
+
+  it("should create a client", async () => {
+    const client = new Client({
+      id: new Id("1"),
+      name: "Client 1",
+      email: "x@x.com",
+      address: "Address 1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const clientRepository = new ClientRepository();
+
+    await clientRepository.add(client);
+
+    const clientDb = await ClientModel.findOne({ where: { id: "1" } });
+
+    expect(clientDb).toBeDefined();
+    expect(clientDb.id).toBe(client.id.id);
+    expect(clientDb.name).toBe(client.name);
+    expect(clientDb.address).toBe(client.address);
+    expect(clientDb.email).toBe(client.email);
+    expect(clientDb.createdAt).toEqual(client.createdAt);
+    expect(clientDb.updatedAt).toEqual(client.updatedAt);
   });
 
   it("should a find a client", async () => {
