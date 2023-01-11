d3a033f043ce4c846799693c5a9bfba87bc3c147
vicente_nascimento <vicente19981@live.com>
2023-01-11 12:18:32 -0300

    feat: implementing find a client

---
+++ src/modules/client-adm/repository/client.repository.ts
@@ -0,0 +1,25 @@
+import Id from "../../@shared/domain/value-object/id.value-object";
+import Client from "../domain/client.entity";
+import ClientGateway from "../gateway/client.gateway";
+import ClientModel from "./client.model";
+
+export default class ClientRepository implements ClientGateway {
+  async add(client: Client): Promise<void> {}
+
+  async find(id: string): Promise<Client> {
+    const client = await ClientModel.findOne({ where: { id } });
+
+    if (!client) {
+      throw new Error("client not found");
+    }
+
+    return new Client({
+      id: new Id(client.id),
+      name: client.name,
+      email: client.email,
+      address: client.address,
+      createdAt: client.createdAt,
+      updatedAt: client.updatedAt,
+    });
+  }
+}
