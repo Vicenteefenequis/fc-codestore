0400ffcacfce369fbc2738df151ead76787445c9
vicente_nascimento <vicente19981@live.com>
2023-01-11 11:12:53 -0300

    feat: create input and output to find product

---
+++ src/modules/store-catalog/usecase/find-product/find-product.dto.ts
@@ -0,0 +1,10 @@
+export interface InputFindProductDto {
+  id: string;
+}
+
+export interface OutputFindProductDto {
+  id: string;
+  name: string;
+  description: string;
+  salesPrice: number;
+}
