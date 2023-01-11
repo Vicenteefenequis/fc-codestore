c3ae7390db357551ea99b3b2f4cae202c6d7d8b1
vicente_nascimento <vicente19981@live.com>
2023-01-11 00:08:28 -0300

    feat: create factory product adm facade factory

---
+++ src/modules/product-adm/factory/facade.factory.ts
@@ -0,0 +1,16 @@
+import ProductAdmFacade from "../facade/product-adm.facade";
+import ProductRepository from "../repository/product.repository";
+import AddProductUseCase from "../usecase/add-product/add-product.usecase";
+import CheckStockUseCase from "../usecase/check-stock/check-stock.usecase";
+
+export default class ProductAdmFacadeFactory {
+  static create() {
+    const productRepository = new ProductRepository();
+    const addProductUseCase = new AddProductUseCase(productRepository);
+    const checkStockUseCase = new CheckStockUseCase(productRepository);
+    return new ProductAdmFacade({
+      addProductUseCase,
+      checkStockUseCase,
+    });
+  }
+}
