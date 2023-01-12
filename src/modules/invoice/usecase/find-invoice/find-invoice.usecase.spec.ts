7c011ea1dbcf59c8f37abb3c339ba1568c19099b
vicente_nascimento <vicente19981@live.com>
2023-01-12 14:32:24 -0300

    test:  should FindInvoiceUseCase find a invoice

---
+++ src/modules/invoice/usecase/find-invoice/find-invoice.usecase.spec.ts
@@ -0,0 +1,55 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import Address from "../../domain/address.value-object";
+import Invoice from "../../domain/invoice";
+import Product from "../../domain/product";
+import InvoiceGateway from "../../gateway/invoice.gateway";
+import FindInvoiceUseCase from "./find-invoice.usecase";
+
+const invoice = new Invoice({
+  id: new Id("1"),
+  address: new Address({
+    city: "City 1",
+    complement: "Complement 1",
+    number: "Number 1",
+    state: "State 1",
+    street: "Street 1",
+    zipCode: "ZipCode 1",
+  }),
+  document: "Document 1",
+  items: [new Product({ id: new Id("1"), name: "Product 1", price: 100 })],
+  name: "Invoice 1",
+});
+
+const MockRepository = (): InvoiceGateway => ({
+  find: jest.fn().mockReturnValue(invoice),
+  generate: jest.fn(),
+});
+
+describe("FindInvoiceUseCase test unit", () => {
+  it("should find a invoice", async () => {
+    const repository = MockRepository();
+    const usecase = new FindInvoiceUseCase(repository);
+
+    const input = {
+      id: "1",
+    };
+
+    const result = await usecase.execute(input);
+
+    expect(result.id).toBe(invoice.id.id);
+    expect(result.name).toBe(invoice.name);
+    expect(result.document).toBe(invoice.document);
+
+    expect(result.city).toBe(invoice.address.city);
+    expect(result.complement).toBe(invoice.address.complement);
+    expect(result.number).toBe(invoice.address.number);
+    expect(result.state).toBe(invoice.address.state);
+    expect(result.street).toBe(invoice.address.street);
+    expect(result.zipCode).toBe(invoice.address.zipCode);
+
+    expect(result.items.length).toBe(1);
+    expect(result.items[0].id).toBe("1");
+    expect(result.items[0].name).toBe("Product 1");
+    expect(result.items[0].price).toBe(100);
+  });
+});
