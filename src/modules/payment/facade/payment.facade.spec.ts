ee0c50f94a6cd03fec0b04e48f4e64b9ac034e17
vicente_nascimento <vicente19981@live.com>
2023-01-12 00:48:36 -0300

    test: change to factory create

--- src/modules/payment/facade/payment.facade.spec.ts
+++ src/modules/payment/facade/payment.facade.spec.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize-typescript";
+import PaymentFacadeFactory from "../factory/payment.facade.factory";
 import TransactionModel from "../repository/transaction.model";
 import TransactionRepository from "../repository/transaction.repository";
 import ProcessPaymentUseCase from "../usecase/process-payment/process-payment.usecase";
@@ -25,9 +26,7 @@ describe("PaymentFacade Test", () => {
   });
 
   it("should create a transaction", async () => {
-    const repository = new TransactionRepository();
-    const usecase = new ProcessPaymentUseCase(repository);
-    const facade = new PaymentFacade(usecase);
+    const facade = PaymentFacadeFactory.create();
 
     const input = {
       orderId: "1",
