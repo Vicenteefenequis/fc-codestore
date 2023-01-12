a8f0d4e7699029ccd5143a469d2135c6867910ba
vicente_nascimento <vicente19981@live.com>
2023-01-12 14:31:40 -0300

    feat: create product entity

---
+++ src/modules/invoice/domain/product.ts
@@ -0,0 +1,26 @@
+import BaseEntity from "../../@shared/domain/entity/base.entity";
+import Id from "../../@shared/domain/value-object/id.value-object";
+
+interface ProductProps {
+  id: Id;
+  name: string;
+  price: number;
+}
+
+export default class Product extends BaseEntity {
+  _name: string;
+  _price: number;
+
+  constructor(props: ProductProps) {
+    super(props.id);
+    this._name = props.name;
+    this._price = props.price;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+  get price(): number {
+    return this._price;
+  }
+}
