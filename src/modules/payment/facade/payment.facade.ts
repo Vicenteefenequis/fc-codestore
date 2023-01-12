a0f8a647faa32a8477a28045107093c1a6cd3882
vicente_nascimento <vicente19981@live.com>
2023-01-12 00:45:43 -0300

    feat: implementing process payment facade

---
+++ src/modules/payment/facade/payment.facade.ts
@@ -0,0 +1,13 @@
+import UseCaseInterface from "../../@shared/usecase/use-case.interface";
+import PaymentFacadeInterface, {
+  InputPaymentFacadeDto,
+  OutputPaymentFacadeDto,
+} from "./facade.interface";
+
+export default class PaymentFacade implements PaymentFacadeInterface {
+  constructor(private readonly processPaymentUseCase: UseCaseInterface) {}
+
+  async process(input: InputPaymentFacadeDto): Promise<OutputPaymentFacadeDto> {
+    return await this.processPaymentUseCase.execute(input);
+  }
+}
