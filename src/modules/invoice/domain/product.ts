9968b1ae619879f37ee19be8b57895f5ae05cdd3
vicente_nascimento <vicente19981@live.com>
2023-01-12 14:47:42 -0300

    refactor: product id optional

--- src/modules/invoice/domain/product.ts
+++ src/modules/invoice/domain/product.ts
@@ -2,7 +2,7 @@ import BaseEntity from "../../@shared/domain/entity/base.entity";
 import Id from "../../@shared/domain/value-object/id.value-object";
 
 interface ProductProps {
-  id: Id;
+  id?: Id;
   name: string;
   price: number;
 }
