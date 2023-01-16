c92d1523854669ad446844698597ed16ae421dd4
vicente_nascimento <vicente19981@live.com>
2023-01-16 10:12:37 -0300

    refactor: table name store products

--- src/modules/store-catalog/repository/product.model.ts
+++ src/modules/store-catalog/repository/product.model.ts
@@ -1,7 +1,10 @@
 import { Column, Model, PrimaryKey, Table } from "sequelize-typescript";
 
-@Table({ tableName: "products", timestamps: false })
-export default class ProductModel extends Model {
+@Table({
+  tableName: "store-products",
+  timestamps: false,
+})
+export class ProductModel extends Model {
   @PrimaryKey
   @Column({ allowNull: false })
   id: string;
@@ -15,3 +18,5 @@ export default class ProductModel extends Model {
   @Column({ allowNull: false })
   salesPrice: number;
 }
+
+export default ProductModel;
